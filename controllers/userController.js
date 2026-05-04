73353190341f2397f3546e81062b2fbec704c028
VBear <ryoitenkaii@gmail.com>
2026-05-04 11:29:52 +0700

    create register user config

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,12 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+//Generate jtw token
+const generateToken = (userId) => {
+    const payload = userId;
+    return jwt.sign(payload, process.env.JWT_SECRET)
+}
 
 export const registerUser = async (req, res) => {
     try {
@@ -16,9 +23,10 @@ export const registerUser = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({name, email, password: hashedPassword})
-
-        
+        const token = generateToken(user._id.toString())
+        res.json({success: true, token})
     } catch (error) {
-
+        console.log(error.message);
+        res.json({success: false, message: error.message})
     }
 }
