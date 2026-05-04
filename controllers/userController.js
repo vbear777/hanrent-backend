47a2fec17aa6ddbfb17c8de0bdbb35dfbd3c51c6
VBear <ryoitenkaii@gmail.com>
2026-05-04 11:23:22 +0700

    add user config for sign in and signup to server

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,24 @@
+import User from "../models/User.js"
+import bcrypt from 'bcrypt'
 
+export const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body
 
-export const 
+        if(!name || !email || !password || password.length < 8){
+            return res.json({success: false, message: 'fill all the fields'})
+        }
+
+        const useExist = await User.findOne({email})
+        if(userExist){
+            return res.json({success: false, message: 'User already exists'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({name, email, password: hashedPassword})
+
+        
+    } catch (error) {
+
+    }
+}
