0665267890bd0d015a29fb525204d9d09c2b24d3
VBear <ryoitenkaii@gmail.com>
2026-05-05 11:15:54 +0700

    set up image kit upload and optimizer image

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -1,5 +1,7 @@
 import User from "../models/User.js";
-
+import fs from "fs";
+import imagekit from "../configs/imageKit..js";
+import Car from "../models/Car.js";
 
 export const changeRoleToOwner = async (req, res) => {
     try {
@@ -20,7 +22,27 @@ export const addCar = async (req, res) => {
         let car = JSON.parse(req.body.carData);
         const imageFile = req.file;
 
-        
+        //upload img to imagekit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        })
+
+        const optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {width: '1280'},
+                {quality: 'auto'},
+                {format: 'webp'}
+            ]
+        });
+
+        const image = optimizedImageUrl;
+        await Car.create({...car, owner: _id, image})
+
+        res.json({ success: true, message: "Car Successfully Added"})
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message })
