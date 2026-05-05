b592a266ec64382a1f07b5696e8c696fb2f1eb79
VBear <ryoitenkaii@gmail.com>
2026-05-05 11:01:40 +0700

    config imagekit

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -11,3 +11,18 @@ export const changeRoleToOwner = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+//API to List Car 
+
+export const addCar = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+} 
