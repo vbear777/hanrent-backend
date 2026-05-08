7815a48919ec8d12b102507d2749566a155bc955
VBear <ryoitenkaii@gmail.com>
2026-05-08 14:45:23 +0700

    create api for status booking and create endpoint booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -87,3 +87,25 @@ export const getOwnerBookings = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+//api to change booking status
+export const changeBookingStatus = async(req, res) => {
+    try {
+        const {_id} = req.user;
+        const {bookingId, status} = req.body
+
+        const booking = await Booking.findById(bookingId)
+
+        if(booking.owner.toString() !== _id.toString()){
+            return res.json({ success: false, message: "Unauthorized"})
+        }
+
+        booking.status = status;
+        await booking.save();
+
+        res.json({ success: true, message: "Successfully Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
