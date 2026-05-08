a9d0c941ecc85527f32b660549bbce4db95bc00c
VBear <ryoitenkaii@gmail.com>
2026-05-08 09:05:40 +0700

    create booking schema model on mongoose

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { addCar, changeRoleToOwner } from "../controllers/ownerController.js";
+import { addCar, changeRoleToOwner, deleteCar, getOwnerCars, toggleCarAvailability } from "../controllers/ownerController.js";
 import upload from "../middleware/multer.js";
 
 const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
 ownerRouter.post("/add-car", upload.single("image"), protect, addCar)
+ownerRouter.post("cars", protect, getOwnerCars)
+ownerRouter.post("toggle-car", protect, toggleCarAvailability)
+ownerRouter.post("delete-car", protect, deleteCar)
 
 export default ownerRouter;
