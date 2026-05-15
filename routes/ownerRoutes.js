24327fa08076fcbccbf6006097f2ff7807fa92dc
VBear <ryoitenkaii@gmail.com>
2026-05-15 07:33:19 +0700

    fix error endpoint

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -7,9 +7,9 @@ const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
 ownerRouter.post("/add-car", upload.single("image"), protect, addCar)
-ownerRouter.post("cars", protect, getOwnerCars)
-ownerRouter.post("toggle-car", protect, toggleCarAvailability)
-ownerRouter.post("delete-car", protect, deleteCar)
+ownerRouter.get("/cars", protect, getOwnerCars)
+ownerRouter.post("/toggle-car", protect, toggleCarAvailability)
+ownerRouter.post("/delete-car", protect, deleteCar)
 
 ownerRouter.get('/dashboard', protect, getDashboardData)
 ownerRouter.post('/update-image', upload.single("image"), protect, updateUserImage)
